c7b513003e5338940f8c067794b2593fcb7ae5b4
C110181138 <93572159+C110181138@users.noreply.github.com>
2021-11-16 14:46:28 +0800

    Add files via upload

---
+++ NKUST/NKUST/NKUST/bmi.java
@@ -0,0 +1,8 @@
+public class bmi {
+    public static void main(String[] args) {
+        float high = 1.75f;
+        int weight = 72;
+        float bmi =   weight/(high*high);
+        System.out.println(bmi);
+    }
+}
